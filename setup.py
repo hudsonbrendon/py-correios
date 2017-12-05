081b1c1f9354b2c21a94ae22a5b9cc64ce8d1ef4
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-12-05 19:47:11 -0300

    Added setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import os
 from setuptools import setup, find_packages
+
+
+DESCRIPTION = "Informações de CEP, encomendas e endereços em poucas linhas de código."
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    pass
 
 
 setup(name='py-correios',
