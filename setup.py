f0b0ed0b9302b8e4afe331a071a80940956fa9ab
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-12-05 18:58:14 -0300

    Add base

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(name='py-correios',
+      version='0.0.1',
+      description='Informações de CEP, encomendas e endereços em poucas linhas de código.',
+      url='https://github.com/hudsonbrendon/py-correios',
+      author='Hudson Brendon',
+      author_email='contato.hudsonbrendon@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['tests*']),
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
